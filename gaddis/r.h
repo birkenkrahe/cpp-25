0a2cfd902477edbfc7f2081d482681fee1fdfb0e
Marcus Birkenkrahe <377079+birkenkrahe@users.noreply.github.com>
2025-08-21 08:53:12 -0500

    Update from thelio

---
+++ gaddis/r.h
@@ -0,0 +1,21 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include <iostream>
+// Namespace declaration
+using namespace std;
+
+// Class declaration
+class Rectangle {
+  // private member data
+private:
+  double width; // rectangle width
+  double length;  // rectangle length
+  // public member functions
+public:
+  void setWidth (double );  // set rectangle width
+  void setLength (double ); // set rectangle length
+  double getWidth() const;  // get rectangle width
+  double getLength() const; // get rectangle length
+  double getArea() const;   // compute and get rectangle area
+};
+#endif
